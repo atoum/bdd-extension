b5b1d8b81de89762145bff68e2f2a602abe59b72
jubianchi <julien.bianchi@pmsipilot.com>
2015-02-21 16:39:33 +0100

    Require test autoloader in main autoloader

--- autoloader.php
+++ autoloader.php
@@ -11,3 +11,5 @@ atoum\autoloader::get()
 	->addClassAlias('mageekguy\atoum\spec', __NAMESPACE__ . '\\spec')
 	->addDirectory(__NAMESPACE__, __DIR__ . DIRECTORY_SEPARATOR . 'classes');
 ;
+
+require_once __DIR__ . '/specs/autoloader.php';
