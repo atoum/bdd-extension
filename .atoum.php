81eb0954f3d7045821e7c8afa3321a5f03782ebd
jubianchi <julien.bianchi@pmsipilot.com>
2015-02-21 23:02:47 +0100

    Update autoloader

--- .atoum.php
+++ .atoum.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 
 use mageekguy\atoum\bdd;
 
